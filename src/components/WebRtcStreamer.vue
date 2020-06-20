59e86d29e261885c9d2630b22859e8a9770e63b7
mpromonet <michel.promonet@free.fr>
2020-06-20 12:35:44 +0200

    fix parameters bindind

--- src/components/WebRtcStreamer.vue
+++ src/components/WebRtcStreamer.vue
@@ -1,15 +1,14 @@
-<script type="module" src="webrtc-streamer-element.js"></script>
 <template>
   <div>
-    <webrtc-streamer url="rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov" webrtcurl="http://webrtc-streamer.herokuapp.com/" muted></webrtc-streamer>
+    <webrtc-streamer :url="webrtcstream" :webrtcurl="webrtcurl" muted></webrtc-streamer>
   </div>
 </template>
 <script>
 export default {
   name: 'WebRtcStreamer',
   props: {
-    webrtcstream: String
+    webrtcstream: String,
     webrtcurl: String
   }
 }
-
+</script>
