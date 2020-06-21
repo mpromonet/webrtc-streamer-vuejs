01c2b48c44e1a9a4b5b140cc14ec869ec072e609
mpromonet <michel.promonet@free.fr>
2020-06-21 11:10:02 +0200

    simplify

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,13 @@
   <div id="app">
     <input v-model="webrtcstream" size="64"><br>
     <input v-model="webrtcurl"   size="64">
-    <WebRtcStreamer 
-        :webrtcstream="webrtcstream"
-        :webrtcurl="webrtcurl" />
+    <webrtc-streamer :url="webrtcstream" :webrtcurl="webrtcurl" muted></webrtc-streamer>
   </div>
 </template>
 
 <script>
-import WebRtcStreamer from './components/WebRtcStreamer.vue'
-
 export default {
   name: 'App',
-  components: {
-    WebRtcStreamer
-  },
   data() {
     return {
       webrtcstream: 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov',
